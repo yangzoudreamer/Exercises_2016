030d901657f6c31e1b16ae2b7fad9b48e5d3f30b
younghust <shanglihust@163.com>
2016-05-03 18:58:04 +0800

    add some algorithms

---
+++ src/MultiplyStrings.java
@@ -0,0 +1,12 @@
+
+public class MultiplyStrings {
+    public String multiply(String num1, String num2) {
+        String res = "";
+        
+        return res;
+    }
+    
+    public static void main(String[] args) {
+		
+	}
+}

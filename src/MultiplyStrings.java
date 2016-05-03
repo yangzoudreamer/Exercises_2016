d04d3636cb6bb5e7fab9345755aaee510f84c0a2
younghust <shanglihust@163.com>
2016-05-03 19:05:05 +0800

    add new

--- src/MultiplyStrings.java
+++ src/MultiplyStrings.java
@@ -6,6 +6,16 @@ public class MultiplyStrings {
         return res;
     }
     
+    public String sum(String num1,String num2){
+    	String sumRes = "";
+    	String flag = "";
+    	String k = "";
+    	String longEle =  num1.length() > num2.length() ? num1 : num2;
+    	
+    	
+    	return sumRes;
+    }
+    
     public static void main(String[] args) {
 		
 	}
